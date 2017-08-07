85e2ac1644ac19a1ed054d6bbfba390ff971b289
José Leonardo Sotelo <jsotelob1330@universidadean.edu.co>
2017-08-06 20:44:50 -0500

    Intercambio inicial de proyecto

---
+++ Suma_2_Números.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/**
+ * Programa para sumar dos Números entre 1 y 100.
+ * 
+ * @author (Juan León-Leonardo Sotelo) 
+ * 
+ */
+public class Suma_2_Números
+{
+    public static void main(String[] args) {
+     double número_1;
+     double número_2;
+     double Suma;
+     
+    Scanner teclado = new Scanner (System.in);
+    System.out.print( "Dos numeros entre 1 y 100, separados por espacio :" );    
+    número_1 =teclado.nextDouble (); número_2 =teclado.nextDouble ();
+    
+    Suma = número_1 + número_2;
+    System.out.println("Suma" + Suma);         
+    }
+}
